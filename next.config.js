2dbe43801bb31cd4fa8a74b48866cb4a4dfaeb03
Oleksandr Tolochko <0504fusion@gmail.com>
2023-11-09 03:35:27 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    distDir: 'dist',
     experimental: {
         outputFileTracingRoot: './db.json',
         webpackBuildWorker: true
