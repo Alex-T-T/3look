169be4107833c13b9de8469ba3039c5b1480ba8e
Oleksandr Tolochko <0504fusion@gmail.com>
2023-11-13 03:36:10 +0200

    updated font src

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,33 @@ import './globals.css';
 import localFont from 'next/font/local';
 
 const Satoshi = localFont({
-    src: '../public/fonts/Satoshi/Fonts/OTF/Satoshi-Regular.otf',
+    src: [
+        {
+            path: '../public/fonts/Satoshi/Fonts/OTF/Satoshi-Light.otf',
+            weight: '200',
+            style: 'normal',
+        },
+        {
+            path: '../public/fonts/Satoshi/Fonts/OTF/Satoshi-Regular.otf',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../public/fonts/Satoshi/Fonts/OTF/Satoshi-Medium.otf',
+            weight: '600',
+            style: 'normal',
+        },
+        {
+            path: '../public/fonts/Satoshi/Fonts/OTF/Satoshi-Bold.otf',
+            weight: '800',
+            style: 'normal',
+        },
+        {
+            path: '../public/fonts/Satoshi/Fonts/OTF/Satoshi-Black.otf',
+            weight: '900',
+            style: 'normal',
+        },
+    ],
     display: 'swap',
     variable: '--satoshi-regular',
 });
